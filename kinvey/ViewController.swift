018734d1cbfc95d44f29a6449309633821544c6f
Daniel Contreras <dfcontr@okstate.edu>
2016-05-16 12:53:49 -0500

    Migrating SS from Parse to KInvey

--- kinvey/ViewController.swift
+++ kinvey/ViewController.swift
@@ -38,6 +38,7 @@ class ViewController: UIViewController {
                         cancelButtonTitle: NSLocalizedString("OK", comment: "OK")
                     )
                     alert.show()
+                    //Return
                 }
             }
         )
